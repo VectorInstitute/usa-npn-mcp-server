d2548cc392161e552cdd85a7a800655b2dd963ce
Nate <lespernate@gmail.com>
2025-03-21 10:31:49 -0400

    Add prompt handling (list and get) functionality to the NPN MCP server

--- src/usa_npn_mcp_server/server.py
+++ src/usa_npn_mcp_server/server.py
@@ -17,7 +17,10 @@ from mcp.server.models import InitializationOptions
 from mcp.server.stdio import stdio_server
 from mcp.types import (
     EmbeddedResource,
+    GetPromptResult,
     ImageContent,
+    Prompt,
+    PromptMessage,
     Resource,
     ResourcesCapability,
     ServerCapabilities,
@@ -30,6 +33,7 @@ from pydantic import AnyUrl
 
 from usa_npn_mcp_server.api_client import APIClient
 from usa_npn_mcp_server.utils.endpoints import NPNTools
+from usa_npn_mcp_server.utils.prompts import PROMPTS, get_prompts
 
 
 logging.basicConfig(
@@ -51,30 +55,17 @@ async def serve() -> None:
         logger.info("Handling list_tools request")
         return [
             Tool(
-                name=NPNTools.Observations.name,
-                description=NPNTools.Observations.description,
-                inputSchema=NPNTools.Observations.input_schema,
-            ),
-            Tool(
-                name=NPNTools.ObservationComment.name,
-                description=NPNTools.ObservationComment.description,
-                inputSchema=NPNTools.ObservationComment.input_schema,
-            ),
-            Tool(
-                name=NPNTools.SummarizedData.name,
-                description=NPNTools.SummarizedData.description,
-                inputSchema=NPNTools.SummarizedData.input_schema,
-            ),
-            Tool(
-                name=NPNTools.MagnitudeData.name,
-                description=NPNTools.MagnitudeData.description,
-                inputSchema=NPNTools.MagnitudeData.input_schema,
-            ),
-            Tool(
-                name=NPNTools.SiteLevelData.name,
-                description=NPNTools.SiteLevelData.description,
-                inputSchema=NPNTools.SiteLevelData.input_schema,
-            ),
+                name=tool.name,
+                description=tool.description,
+                inputSchema=tool.input_schema,
+            )
+            for tool in [
+                NPNTools.Observations,
+                NPNTools.ObservationComment,
+                NPNTools.MagnitudeData,
+                NPNTools.SiteLevelData,
+                NPNTools.SummarizedData,
+            ]
         ]
 
     @server.list_resources()
@@ -114,6 +105,32 @@ async def serve() -> None:
             ),
         ]
 
+    @server.list_prompts()
+    async def handle_list_prompts() -> list[Prompt]:
+        logger.info("Handling list_prompts request")
+        return get_prompts()
+
+    @server.get_prompt()
+    async def handle_get_prompt(
+        prompt_name: str, arguments: Dict[str, str] | None
+    ) -> GetPromptResult:
+        logger.info(f"Handling get_prompt request for {prompt_name}")
+        if prompt_name not in PROMPTS:
+            raise ValueError(f"Prompt '{prompt_name}' not found.")
+        if arguments is None:
+            arguments = {}
+        template = str(PROMPTS[prompt_name]["template"])
+        prompt = template.format(**arguments)
+        return GetPromptResult(
+            description=f"Demo template for {arguments}",
+            messages=[
+                PromptMessage(
+                    role="user",
+                    content=TextContent(type="text", text=prompt.strip()),
+                )
+            ],
+        )
+
     @server.read_resource()
     async def handle_read_resource(
         uri: AnyUrl,
@@ -122,7 +139,6 @@ async def serve() -> None:
         logger.info(f"Handling read_resource request for URI: {uri}")
         # Throw error if the URI scheme is not supported
         if uri.scheme != "npn-mcp":
-            logger.error(f"Unsupported URI scheme: {uri.scheme}")
             raise ValueError(f"Unsupported URI scheme: {uri.scheme}")
         name = str(uri).replace("npn-mcp://", "")
         last_resource = api_client.read_last_response(name=name)
@@ -159,7 +175,6 @@ async def serve() -> None:
             case NPNTools.SiteLevelData.name:
                 await api_client.query_api(NPNTools.SiteLevelData.endpoint, arguments)
             case _:
-                logger.error(f"Unknown tool requested: {name}")
                 raise ValueError(f"Unknown tool requested: {name}")
         # Notify client of resource update
         await server.request_context.session.send_resource_updated(
